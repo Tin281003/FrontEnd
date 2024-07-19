0b49282c1ef9651a8d5f6bc62d9a10253d672209
Thankluan <21130100@st.hcmuaf.edu.vn>
2024-07-19 15:51:17 +0700

    Commit

--- my-app/src/pages/Detail.js
+++ my-app/src/pages/Detail.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import ReadLink from "../components/ReadLink ";
+import ReadLink from "../components/ReadHtml ";
 
 const Detail =() =>{
     return(
