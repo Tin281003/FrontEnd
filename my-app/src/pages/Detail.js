37d1a7ba2eddc7997184bef748a825d6c7973230
Thankluan <21130100@st.hcmuaf.edu.vn>
2024-07-19 14:40:10 +0700

    Commit

---
+++ my-app/src/pages/Detail.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ReadLink from "../components/ReadLink ";
+
+const Detail =() =>{
+    return(
+        <div className="text-gray-700 pt-9 sm:pt-10 text-left ">
+            <Header> </Header>
+                <ReadLink></ReadLink>
+                <Footer></Footer>
+        </div>
+    )
+}
+export default Detail
