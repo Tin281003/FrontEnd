90518de8940db1d0dbc8edc66b5f7026e12a0ac0
hongtien0221 <hoti9481@gmail.com>
2024-07-20 12:52:43 +0700

    edit component(add congnghe)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,16 @@ import '~@fortawesome/fontawesome-free/css/fontawesome.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-// import Dulich from "./pages/Dulich";
 import Home from "./pages/Home";
 import News from "./pages/News";
 import Layout from "./pages/Layout";
 
-// import Thethao from "./pages/Thethao";
+ import Thethao from "./pages/Thethao";
  import CongNghe from "./pages/CongNghe";
 
 
 import Thethao from "./pages/Thethao";
-// import CongNghe from "./pages/CongNghe";
+import CongNghe from "./pages/CongNghe";
 import AmThuc from "./pages/AmThuc";
 import VanHoa from "./pages/VanHoa";
 // import DoiSongVanHoa from "./pages/DoiSongVanHoa";
@@ -24,11 +23,11 @@ import VanHoa from "./pages/VanHoa";
 import OtoXeMay from "./pages/OtoXeMay";
 
 import KhoaHocDoiSong from "./pages/KhoaHocDoiSong";
-// import Search from "./pages/Search";
-// import Recent from "./pages/Recent";
-// import Saved from "./pages/Saved";
+ import Search from "./pages/Search";
+ import Recent from "./pages/Recent";
+ import Saved from "./pages/Saved";
 
-// import KhoaHocDoiSong from "./pages/KhoaHocDoiSong";
+ import KhoaHocDoiSong from "./pages/KhoaHocDoiSong";
  import Search from "./pages/Search";
  import Recent from "./pages/Recent";
  import Saved from "./pages/Saved";
@@ -41,26 +40,26 @@ function App() {
           <Route element={<Layout></Layout>}>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
-            {<Route path="/van-hoa" element={<VanHoa />}></Route>}
-            {/*<Route path="/the-thao" element={<TheThao />}></Route>*/}
-            {<Route path="/cong-nghe" element={<CongNghe />}></Route>}
-            {<Route path="/am-thuc" element={<AmThuc />}></Route>}
+            <Route path="/van-hoa" element={<VanHoa />}></Route>
+            <Route path="/the-thao" element={<TheThao />}></Route>
+            <Route path="/cong-nghe" element={<CongNghe />}></Route>
+            <Route path="/am-thuc" element={<AmThuc />}></Route>
             {/*<Route*/}
-            {/*    path="/doi-song-van-hoa"*/}
+            {/*   path="/doi-song-van-hoa"*/}
             {/*    element={<DoiSongVanHoa />}*/}
             {/*></Route>*/}
-            {/*<Route*/}
-            {/*    // path="/giai-tri-sao"*/}
-            {/*    // element={<DuLich />}*/}
-          {/*  ></Route>*/}
-          {  <Route path="/chuyen-the-thao" element={<ChuyenTheThao />}></Route>}
-          {/*  <Route path="/the-thao-24h" element={<TheThao24h />}></Route>*/}
-          {  <Route path="/o-to-xe-may" element={<OtoXeMay />}></Route>}
-          {/*  <Route path="/khoa-hoc-doi-song" element={<KhoaHocDoiSong />}></Route>*/}
-          {  <Route path="/search" element={<Search />}></Route>}
-          {  <Route path="/tin-da-xem" element={<Recent />}></Route>}
-          {  <Route path="/tin-da-luu" element={<Saved />}></Route>}
-          {  <Route path="/news/:newsId" element={<News />}></Route>}
+            <Route
+                path="/giai-tri-sao"
+                element={<DuLich />}
+            ></Route>
+            <Route path="/chuyen-the-thao" element={<ChuyenTheThao />}></Route>
+            <Route path="/the-thao-24h" element={<TheThao24h />}></Route>
+            <Route path="/o-to-xe-may" element={<OtoXeMay />}></Route>
+            <Route path="/khoa-hoc-doi-song" element={<KhoaHocDoiSong />}></Route>
+            <Route path="/search" element={<Search />}></Route>
+            <Route path="/tin-da-xem" element={<Recent />}></Route>
+            <Route path="/tin-da-luu" element={<Saved />}></Route>
+            <Route path="/news/:newsId" element={<News />}></Route>
           </Route>
         </Routes>
       </div>
